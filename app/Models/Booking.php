3da5ba79f134945922e264d2b8208d113e8c4e7f
Marviel Billante <marvielb@gmail.com>
2023-12-28 08:32:24 +0800

    Added Laravel IDE Helper

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperBooking
+ */
 class Booking extends Model
 {
     use HasFactory;
--- app/Models/Shuttle.php
+++ app/Models/Shuttle.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperShuttle
+ */
 class Shuttle extends Model
 {
     use HasFactory;
