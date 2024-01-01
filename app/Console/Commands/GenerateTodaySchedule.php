b0b92248c925ae3f92eca35fc63604148c78c7fe
Marviel Billante <marvielb@gmail.com>
2024-01-01 11:01:50 +0800

    Created a script for generating today's schedules

---
+++ app/Console/Commands/GenerateTodaySchedule.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Booking;
+use App\Models\Location;
+use App\Models\Shuttle;
+use App\Models\ShuttleSchedule;
+use App\Models\TimeSlot;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
+
+class GenerateTodaySchedule extends Command
+{
+    use WithFaker;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setUpFaker();
+    }
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gen-sched';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command will generate today\'s schedule.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $shuttles = Shuttle::all();
+        $locations = Location::all();
+        $locationIds = $locations->map(fn ($location) => $location['id']);
+        $possibleRoutes = collect(Arr::crossJoin($locationIds, $locationIds))
+            ->filter(fn ($route) => $route[0] != $route[1])
+            ->values();
+        $users = User::all();
+        $shuttles->each(function ($shuttle, $index) use ($possibleRoutes, $users) {
+            $timeslots = TimeSlot::all();
+            $timeslots->each(function ($timeslot, $timeslotIndex) use ($index, $shuttle, $possibleRoutes, $users) {
+                $routeIndex = $index % count($possibleRoutes);
+                $schedule = ShuttleSchedule::factory()->create([
+                    'shuttle_id' => $shuttle['id'],
+                    'time_slot_id' => $timeslot['id'],
+                    'from_location_id' => $possibleRoutes[$routeIndex][0],
+                    'to_location_id' => $possibleRoutes[$routeIndex][1],
+                ]);
+                $randomBookings = $this->faker->randomElement(range(0, $shuttle['capacity']));
+                Booking::factory()->count($randomBookings)->create([
+                    'shuttle_schedule_id' => $schedule['id'],
+                    'user_id' => $this->faker->randomElement($users),
+                ]);
+            });
+        });
+    }
+}
